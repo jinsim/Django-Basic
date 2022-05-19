5ee7bc98ecbf7e99187c248ab6875cadbf34b213
jinsim <simsime@korea.ac.kr>
2022-05-19 23:58:37 +0900

    CRUD - Create

--- project/app/views.py
+++ project/app/views.py
@@ -6,3 +6,12 @@ def home(request):
     posts = Post.objects.all()
 
     return render(request, 'home.html', { 'posts' : posts })
+
+def new(request):
+    if request.method == 'POST':
+        Post.objects.create(
+            title = request.POST['title'], 
+            content = request.POST['content']
+        )
+        
+    return render(request, 'new.html')
