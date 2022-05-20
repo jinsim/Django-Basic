e5ec7b57aeb8f87b1aa0dc02779af6ca37a1127a
jinsim <simsime@korea.ac.kr>
2022-05-20 19:44:53 +0900

    CRUD - Update

--- project/app/views.py
+++ project/app/views.py
@@ -21,3 +21,15 @@ def detail(request, post_pk):
     post = Post.objects.get(pk=post_pk)
 
     return render(request, 'detail.html', {'post': post})
+    
+def edit(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    
+    if request.method == 'POST':
+        Post.objects.filter(pk=post_pk).update(
+            title = request.POST['title'],
+            content = request.POST['content']
+        )
+        return redirect('detail', post_pk)
+
+    return render(request, 'edit.html', {'post': post})
